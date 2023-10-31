c490c035a258ed78273b8cde2c31bf7e770951a9
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2023-10-31 03:23:13 -0400

    Added a couple neighbourhood datasets

--- data_handling/functions.py
+++ data_handling/functions.py
@@ -2,9 +2,14 @@ import os, sys
 import pandas
 import shapely
 import geopandas
+import ast
+import re
 
 CRS = "urn:ogc:def:crs:OGC:1.3:CRS84"
 
+EPSG = 32610
+
+REGEX_FLOAT = r"([-]?[0-9]*[.]?[0-9]+)"
 
 
 def get_data_path(file_name: str):
@@ -16,6 +21,15 @@ def get_data_path(file_name: str):
 
 
 
+def get_raw_path(file_name: str):
+    """Returns path to a file in the raw data folder."""
+    curr_dir = os.path.dirname(__file__)
+    main_dir = os.path.dirname(curr_dir)
+    raw_dir  = os.path.join(main_dir, 'raw_data')
+    return os.path.join(raw_dir, file_name)
+
+
+
 def store_context_data(gdf: geopandas.GeoDataFrame, file_name: str) -> None:
     """Stores a geopandas.GeoDataFrame to a .csv file."""
     file_path = get_data_path(file_name)
@@ -23,6 +37,16 @@ def store_context_data(gdf: geopandas.GeoDataFrame, file_name: str) -> None:
     pandas.DataFrame(gdf.to_crs(CRS)).to_csv(file_path)
 
     return
+
+
+
+def print_df_columns(df: pandas.DataFrame) -> None:
+  """Prints the column names and dtypes of a pandas.DataFrame (or derived class)."""
+  max_len = max(len(column) for column in df.columns)
+
+  for column, dtype in zip(df.columns, df.dtypes):
+    spaces = ' ' * (max_len - len(column))
+    print(f"{column}{spaces} | {dtype}")
 
 
 
@@ -37,6 +61,43 @@ def open_geopandas_csv(file_path: str, **kwargs) -> geopandas.GeoDataFrame:
     gdf.crs = CRS
 
     return gdf
+
+
+
+def get_gdf_using_geom_dict(df: pandas.DataFrame) -> geopandas.GeoDataFrame:
+    """Translate dataframe from Toronto Open Data source, using geom_dict_to_shape to transform the geometry column."""
+    ## Transform the geometry column
+    print("Processing geometry column...")
+    geometry = df['geometry'].apply(geom_dict_to_shape)
+
+    ## Typecast to geopandas GeoDataFrame
+    print("Creating GeoDataFrame")
+    gdf = geopandas.GeoDataFrame(df, geometry=geometry)
+    gdf.crs = CRS
+
+    return gdf
+
+
+
+def geom_dict_to_shape(geom_dict_str: str):
+    """Transforms a geometry column in 'dict' string format, to a shapely shape object."""
+    ## Extract the shape type, and make it uppercase
+    shape_type  = re.search(r"'type': '([a-zA-Z]+)'", geom_dict_str).group(1).upper()
+
+    ## Extract the coordinates dictionary
+    shape_coord = re.search(r"'coordinates': (.+)}", geom_dict_str).group(1)
+
+    ## Extract any lists of values in shape_coord (ie. form of '[{value_1}, {value_2}]' -> '{value_1} {value)_2}')
+    shape_coord = re.sub(f"\[{REGEX_FLOAT}, {REGEX_FLOAT}\]", r"\1 \2", shape_coord)
+
+    ## Replace any square brackets with round brackets
+    shape_coord = shape_coord.replace('[', '(').replace(']', ')')
+
+    ## Combine string segments
+    shape_string = f"{shape_type} {shape_coord}"
+
+    ## Use shapely.wkt.loads to create shape from string
+    return shapely.wkt.loads(shape_string)
 
 
 
@@ -66,7 +127,7 @@ def get_neighbourhoods() -> geopandas.GeoDataFrame:
     file_dir = os.path.join(data_dir, file_name)
 
     ## Open the file from .csv format
-    return open_geopandas_csv(file_dir)
+    return open_geopandas_csv(file_dir, index_col='AREA_CODE')
 
 
 
@@ -78,7 +139,7 @@ def add_neighbourhood_column(gdf: geopandas.GeoDataFrame, nbhood_gdf: geopandas.
      - [nbhood_gdf]: The neighbourhood geopandas.GeoDataFrame.
      - [how]:        How to join the data (default 'inner' - only keep rows where a neighbourhood is found for gdf, to keep rows with no neighbourhood, use 'left')
      - [predicate]:  How to determine which rows to match.
-       |->           Potential values for predicate include:
+       |->           Potential values for predicate include:df
          |-> 'within'     -> Use this when gdf contains points in the geometry column
          |-> 'intersects' -> Use this when gdf contains lines/regions in the geometry column
     """
@@ -90,6 +151,7 @@ def add_neighbourhood_column(gdf: geopandas.GeoDataFrame, nbhood_gdf: geopandas.
         nbhood_gdf = get_neighbourhoods()
 
     ## Use the spacial join function, for all items within the neighbourhood
+    print(f"Joining now using: 'how={how}', 'predicate={predicate}'")
     joined = geopandas.sjoin(gdf_copy.to_crs(CRS), nbhood_gdf.to_crs(CRS), how = how, predicate = predicate,)
 
 
--- raw_data/readme.md
+++ raw_data/readme.md
@@ -2,3 +2,7 @@
 | :---: | :----: | :---------: | :-----------: |
 | Traffic Collisions - 4326.csv | [Toronto Open Data](https://open.toronto.ca/dataset/police-annual-statistical-report-traffic-collisions/) | Police record of car collisions in Toronto from 2014 | 11 Oct. 2023 |
 | Neighbourhoods - 4326.geojson | [Toronto Open Data](https://open.toronto.ca/dataset/neighbourhoods/) | Neighbourhoods defined for Toronto | 24 Oct. 2023 |
+| raw-data-2010-2019.csv | [Toronto Open Data](https://open.toronto.ca/dataset/traffic-volumes-at-intersections-for-all-modes/) | Traffic Volumes | 11 Oct. 2023 |
+| Centreline - Version 2 - 4326.csv | [Toronto Open Data](https://open.toronto.ca/dataset/toronto-centreline-tcl/) | Centrelines - features such as streets, walkways, highways, rivers, etc. | 11 Oct. 2023|
+| Centreline Intersection - 4326.csv | [Toronto Open Data](https://open.toronto.ca/dataset/toronto-centreline-tcl/) | Intersections | 11 Oct. 2023 |
+| climate-daily-2.csv | Unsure | Weather data... | 30 Oct. 2023 |
