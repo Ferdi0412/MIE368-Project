a03d4d2380a0a8dbc35388b34500a4b3d0ce5b9e
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2023-10-31 00:01:19 -0400

    Cleaned Traffic Collisions Training Data

--- data_handling/neighbourhood.py
+++ data_handling/neighbourhood.py
@@ -3,13 +3,79 @@ import pandas
 import shapely
 import geopandas
 
-def open_geopandas_csv(file_path: str) -> geopandas.GeoDataFrame:
-    df = pandas.read_csv(file_path)
-    return geopandas.GeoDataFrame(df.drop('geometry', axis=1), df['geometry'].apply(shapely.wkt.loads))
+CRS = "urn:ogc:def:crs:OGC:1.3:CRS84"
 
-def get_neighbourhoods() -> geopandas.GeoDataFrame:
+def open_geopandas_csv(file_path: str, **kwargs) -> geopandas.GeoDataFrame:
+    ## Open the .csv using pandas
+    df = pandas.read_csv(file_path, **kwargs)
+
+    ## Transform the dataset into a geopandas dataset
+    gdf = geopandas.GeoDataFrame(df.drop('geometry', axis=1), geometry=df['geometry'].apply(shapely.wkt.loads))
+
+    ## Apply .crs to keep stuff somewhat standardized
+    gdf.crs = CRS
+
+    return gdf
+
+
+
+def get_collisions() -> geopandas.GeoDataFrame:
+    """Returns the dataframe containing collision data."""
+    file_name = 'Traffic Collisions - Cleaned.csv'
+
+    ## Path to the file
     curr_dir = os.path.dirname(__file__)
     main_dir = os.path.dirname(curr_dir)
-    data_dir = os.path.join(main_dir, 'constext-data')
-    file_dir = os.path.join(data_dir, 'Neighbourhood.csv')
+    data_dir = os.path.join(main_dir, 'train_data')
+    file_dir = os.path.join(data_dir, file_name)
+
+    ## Return the geopandas dataframe
+    return open_geopandas_csv(file_dir, index_col = 0)
+
+
+
+def get_neighbourhoods() -> geopandas.GeoDataFrame:
+    """Returns the dataframe containing neighbourhoods."""
+    file_name = 'Neighbourhood.csv'
+
+    ## Path to the file
+    curr_dir = os.path.dirname(__file__)
+    main_dir = os.path.dirname(curr_dir)
+    data_dir = os.path.join(main_dir, 'context_data')
+    file_dir = os.path.join(data_dir, file_name)
+
+    ## Open the file from .csv format
     return open_geopandas_csv(file_dir)
+
+
+
+def add_neighbourhood_column(gdf: geopandas.GeoDataFrame, nbhood_gdf: geopandas.GeoDataFrame = None, *, how = 'inner', predicate = 'within') -> geopandas.GeoDataFrame:
+    """Adds a column named 'Neighbourhood', containing the neighbourhood IDs.
+
+    PARAMS:
+     - gdf:          The geopandas.GeoDataFrame with data to assign.
+     - [nbhood_gdf]: The neighbourhood geopandas.GeoDataFrame.
+     - [how]:        How to join the data (default 'inner' - only keep rows where a neighbourhood is found for gdf, to keep rows with no neighbourhood, use 'left')
+     - [predicate]:  How to determine which rows to match.
+       |->           Potential values for predicate include:
+         |-> 'within'     -> Use this when gdf contains points in the geometry column
+         |-> 'intersects' -> Use this when gdf contains lines/regions in the geometry column
+    """
+
+    ## Copy gdf input, in case input is a filtered GeoDataFrame object, instead of a GeoDataFrame instance
+    gdf_copy = gdf.copy()
+
+    if not nbhood_gdf:
+        nbhood_gdf = get_neighbourhoods()
+
+    ## Use the spacial join function, for all items within the neighbourhood
+    joined = geopandas.sjoin(gdf_copy.to_crs(CRS), nbhood_gdf.to_crs(CRS), how = how, predicate = predicate,)
+
+
+    ## When how == 'inner', some columns may be filtered out. Filter these columns in the output dataset.
+    gdf_copy = gdf_copy.loc[joined.index]
+
+    ## Neighbourhood SHOULD be stored as the index of nbhood_gdf
+    gdf_copy['Neighbourhood'] = joined['index_right']
+
+    return gdf_copy
